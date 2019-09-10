56fbb312d2be2794415e0243dd3bb0ce79314766
tusharjaiswal2k <tusharjaiswal6999@gmail.com>
2019-09-10 21:15:13 +0530

    third commit

--- snakeGame.js
+++ snakeGame.js
@@ -2,59 +2,65 @@ var canvas=document.getElementById('snakeGame');        //linking canvas
 var ctx=canvas.getContext('2d');         //it provides methods to draw  on canvas
 var snakeCordinates=[{xaxis:200,yaxis:200},{xaxis:190,yaxis:200},{xaxis:180,yaxis:200},{xaxis:170,yaxis:200},{xaxis:160,yaxis:200},]; //cordinates for snake
 
-var dx=0,dy=0,upwardMovement,rightMovement,downwardMovement,leftMovement,foodxaxis,foodyaxis,snakeTail,a=0,score=0;//variable declaration
-
+var dx=0,dy=0,upwardMovement,rightMovement,downwardMovement,leftMovement,foodxaxis,foodyaxis,snakeTail,a=40,score=0;//variable declaration
+var flag = true;
 // formation of snake
 
 function snake() {
  snakeTail=snakeCordinates.length-1; //snake tail
 	      for (var i =0;i<snakeTail;i++) {
 	           ctx.fillStyle="blue";
+	           ctx.strokeStyle = "#fff";
 	           ctx.fillRect(snakeCordinates[i].xaxis,snakeCordinates[i].yaxis,10,10);  //printing sanke cordinates
-	           ctx.strokeRect(snakeCordinates[i].xaxis,snakeCordinates[i].yaxis,10,10);  //printing snake boundries
- 	    	   ctx.clearRect(snakeCordinates[snakeTail].xaxis-1,snakeCordinates[snakeTail].yaxis-1,12,12);  //clearing snake tail
+               ctx.strokeRect(snakeCordinates[i].xaxis,snakeCordinates[i].yaxis,10,10);  //printing snake boundries
+ 	    	   ctx.clearRect(snakeCordinates[snakeTail].xaxis,snakeCordinates[snakeTail].yaxis,10,10);  //clearing snake tail
           }
 }
 snake();  //forming first snake
 
 // movement of snake by arrow keys
+	window.addEventListener("keyup",(evt)=>{ 
+	switch(evt.keyCode){
+		case 38: if(flag){
+					if (a!=20) {
+			       	 dx=0;                    
+			         dy=-10;
+			         stop();
+			         upwardMovement=setInterval(movement,100);
+			         a=10; }
+				}		        
+	 	break;
+		case 40:if(flag){
+				 	if (a!=10) {
+			         dx=0;
+			         dy=10;
+			         stop();
+			         downwardMovement=setInterval(movement,100);
+			         a=20;}
+				} 
+		break;
+		case 37: if(flag){
+					if (a!=40) {
+			         dx=-10;
+			         dy=0;
+			         stop();
+			         leftMovement=setInterval(movement,100);
+			         a=30; }
+				}
+		break;
+		case 39: if(flag){
+					if (a!=30) {
+			         dx=10;
+			         dy=0;
+			         stop();
+			         rightMovement=setInterval(movement,100);
+			         a=40;}
+				}
+		break;
+	        }
+	});
 
-window.addEventListener("keyup",(evt)=>{ 
-switch(evt.keyCode){
-	case 38: if (a!=20) {
-	       	 dx=0;                    
-	         dy=-10;
-	         stop();
-	         upwardMovement=setInterval(movement,100);
-	         a=10; }
-	        
- 	break;
-	case 40: if (a!=10) {
-	         dx=0;
-	         dy=10;
-	         stop();
-	         downwardMovement=setInterval(movement,100);
-	         a=20;}
-	         
-	break;
-	case 37: if (a!=40) {
-	         dx=-10;
-	         dy=0;
-	         stop();
-	         leftMovement=setInterval(movement,100);
-	         a=30; }
-	        
-	break;
-	case 39: if (a!=30) {
-	         dx=10;
-	         dy=0;
-	         stop();
-	         rightMovement=setInterval(movement,100);
-	         a=40;}
-	         
-	break;
-        }
-});
+
 
 // SnakeMovement
 
@@ -88,15 +94,15 @@ function stop() {
 
 function food(){
 	 foodxaxis=Math.floor(Math.random()*10)*40;
-	 foodyaxis=Math.floor(Math.random()*10)*40;
+  	 foodyaxis=Math.floor(Math.random()*10)*40;
      ctx.fillStyle="black";
-	 ctx.fillRect(foodxaxis,foodyaxis,10,10);
-	 ctx.strokeRect(foodxaxis,foodyaxis,10,10);
-	
+	 ctx.fillRect(foodxaxis,foodyaxis,9,9);
+		
  //to avoid food on snake body
 
-		 for (var i=1;i<snakeTail; i++) {
+		 for (var i=0;i<snakeTail+1; i++) {
           if (foodxaxis==snakeCordinates[i].xaxis && foodyaxis==snakeCordinates[i].yaxis) { 
+                   ctx.clearRect(foodxaxis,foodyaxis,10,10);      
                    food();
                    break; 
              }	 	
@@ -138,10 +144,17 @@ function gameOver(){
 //printing GameOver
 
 function printGameOver(){ 
+    flag = false;
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.font="30px aerial";
-    ctx.fillText("GAME OVER",0,200);
+    ctx.font="20px aerial";
+    ctx.fillText("GAME OVER",130,200);
+    ctx.fillText("' PRESS ENTER ' TO RESTART",70,230)
     stop();
+    window.addEventListener("keyup",(e)=>{
+    	if(e.keyCode == 13){
+    		location.reload()
+    	}
+    })
 }  
 
 // score 
